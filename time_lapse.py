36439293c4292b41bf3cdd0293d71bc62617b859
harsha1597 <harsha159792@gmail.com>
2021-03-14 12:30:19 +0530

    Update time_lapse.py

--- time_lapse.py
+++ time_lapse.py
@@ -1,9 +1,11 @@
 import os
 inp_time = input('Enter length of time lapse in mins : ') 
 inp_time=int(float(inp_time)*60*1000) #Convert to milliseconds 
-frame_period=2*1000 #Take an image every 2 seconds
+
+frame_freq=input("Enter time between each snap in secs :")
+frame_period=int(frame_freq)*1000 
 total_pics=len(str(int(inp_time//frame_period + 1))) #Number of digits needed when numbering the images 
 file_name="~/Desktop/bird_time/image%0"+str(total_pics)+"d.jpg" 
 command="raspistill -t "+str(inp_time)+" -tl "+str(frame_period)+" -o "+file_name
-print(command)
+#print(command)
 os.system(command)
